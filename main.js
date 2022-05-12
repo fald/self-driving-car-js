30d8106033bad066bdf3375d21af88c5f96b62ac
Firas <4348649+fald@users.noreply.github.com>
2022-05-12 15:19:06 -0400

    The beginning of our car.

--- main.js
+++ main.js
@@ -2,3 +2,7 @@ const canvas = document.getElementById('myCanvas');
 
 canvas.height = window.innerHeight;
 canvas.width = 200; // Just a narrow road, whee.
+
+const context = canvas.getContext('2d');
+const car = new Car(100, 100, 30, 50);
+car.draw(context);
