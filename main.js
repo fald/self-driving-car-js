c16832ee1e93235c7b17f9d258551889f0eb5782
Firas <4348649+fald@users.noreply.github.com>
2022-05-12 15:58:26 -0400

    Animation

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 const canvas = document.getElementById('myCanvas');
 
-canvas.height = window.innerHeight;
 canvas.width = 200; // Just a narrow road, whee.
 
 const context = canvas.getContext('2d');
 const car = new Car(100, 100, 30, 50);
-car.draw(context);
+
+animate();
+
+function animate() {
+    //context.clearRect(0, 0, canvas.width, canvas.height);
+    car.update();
+    canvas.height = window.innerHeight;
+    car.draw(context);
+    requestAnimationFrame(animate);
+}
