b0b196eb64b542f28fa66d7175cf03f2e64563dd
fald <firas.aldabbas+git@gmail.com>
2022-05-18 17:13:02 -0400

    Removed useless line.

--- car.js
+++ car.js
@@ -60,6 +60,5 @@ class Car {
         // Inverted cos/sin since the unit circle is rotated 90 degrees
         this.x += Math.sin(this.angle) * this.speed;
         this.y += Math.cos(this.angle) * this.speed;
-        // this.y -= this.speed;
     }
 }
