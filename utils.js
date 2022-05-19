dd0b11a0956694ce67fa7835edad4d94a9083669
fald <firas.aldabbas+git@gmail.com>
2022-05-19 16:57:15 -0400

    Utils: Footprint for getIntersection to be used in sensor.

--- utils.js
+++ utils.js
@@ -1,3 +1,9 @@
 function lerp(A, B, t) {
     return A + (B - A) * t;
 }
+
+
+function getIntersection(A, B, C, D) {
+    // TODO
+    // given two segments, find the intersection point and the offset.
+}
