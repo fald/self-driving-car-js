bfe355b384e5176765c2e86a1baa4b49ce2e0d97
fald <firas.aldabbas+git@gmail.com>
2022-05-18 22:17:33 -0400

    Road: Borders in constructor.

--- road.js
+++ road.js
@@ -11,6 +11,16 @@ class Road {
         const inf = 1000000;
         this.top = -inf;
         this.bottom = inf;
+
+        const topLeft = {x: this.left, y: this.top};
+        const topRight = {x: this.right, y: this.top};
+        const bottomLeft = {x: this.left, y: this.bottom};
+        const bottomRight = {x: this.right, y: this.bottom};
+
+        this.borders = [
+            [topLeft, bottomLeft],
+            [topRight, bottomRight]
+        ];
     }
 
 
