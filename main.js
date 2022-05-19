6675450a8ae4c0241ee1c72f36ca582456c9a6d6
fald <firas.aldabbas+git@gmail.com>
2022-05-18 21:29:18 -0400

    Road: integrated into main.js

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const canvas = document.getElementById('myCanvas');
 canvas.width = 200; // Just a narrow road, whee.
 
 const context = canvas.getContext('2d');
+const road = new Road(canvas.width / 2, canvas.width);
 const car = new Car(100, 100, 30, 50);
 
 animate();
@@ -10,7 +11,9 @@ animate();
 function animate() {
     //context.clearRect(0, 0, canvas.width, canvas.height);
     car.update();
+
     canvas.height = window.innerHeight;
+    road.draw(context);
     car.draw(context);
     requestAnimationFrame(animate);
 }
