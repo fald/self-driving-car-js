faae6260ae5dd51aa4b2dfb2e33553ce0c3fc929
fald <firas.aldabbas+git@gmail.com>
2022-05-19 12:46:42 -0400

    Sensor: (Untested) Update method to generate rays.

--- sensor.js
+++ sensor.js
@@ -7,4 +7,31 @@ class Sensor {
 
         this.rays = [];
     }
+
+    update() {
+        this.rays = []; // clear the rays
+        
+        for (let i = 0; i < this.rayCount; i++) {
+            const rayAngle = lerp(
+                this.raySpread / 2,
+                -this.raySpread / 2,
+                i / (this.rayCount - 1) // i doesn't become rayCount, so -1 
+            );
+
+            const startPoint = {
+                x: this.car.x,
+                y: this.car.y
+            }
+            const endPoint = {
+                x: startPoint.x + this.rayLength * Math.sin(rayAngle) * this.rayLength,
+                y: startPoint.y + this.rayLength * Math.cos(rayAngle) * this.rayLength
+            }
+
+            this.rays.push([startPoint, endPoint]);
+        }
+    }
+
+    draw(context) {
+        
+    }
 }
