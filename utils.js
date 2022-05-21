06a3a8c81352aac1e9dfcbc6f0ccf70068a969e0
fald <4348649+fald@users.noreply.github.com>
2022-05-20 23:33:49 -0400

    O(N^2) poly inttersect lmao.

--- utils.js
+++ utils.js
@@ -23,3 +23,22 @@ function getIntersection(A, B, C, D) {
 
     return null;
 }
+
+
+function polyIntersect(poly_1, poly_2) {
+    // There's probably a better way...
+    for (let i = 0; i < poly_1.length; i++) {
+        for (let j = 0; i < poly_2.length; j++) {
+            const touch = getIntersection(
+                poly_1[i],
+                poly_1[(i + 1) % poly_1.length],
+                poly_2[j],
+                poly_2[(j + 1) % poly_2.length]
+            )
+            if (touch) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
