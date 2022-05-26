60ee7ba7f578adf32dadbf9237f796830b138200
fald <4348649+fald@users.noreply.github.com>
2022-05-26 16:36:09 -0400

    Main: bestCar now defined at beginning and updated later, allowing global access.

--- main.js
+++ main.js
@@ -12,6 +12,8 @@ const N = 100;
 const road = new Road(carCanvas.width / 2, carCanvas.width * 0.9);
 const cars = generateCars(N);
 
+let bestCar = cars[0];
+
 const traffic = [
     new Car(road.getLaneCenter(1), -100, 30, 50, "DUMMY"),
 ];
@@ -52,7 +54,7 @@ function animate() {
     }
 
     // Find the furthest forward car.
-    const bestCar = cars.find(
+    bestCar = cars.find(
         c => c.y == Math.min(
             ...cars.map(c => c.y)
         )
