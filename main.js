465d947fa971d1fac7f1c2195a42d83e475f114b
fald <4348649+fald@users.noreply.github.com>
2022-05-26 14:49:45 -0400

    Main: primary car is now AI.

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ canvas.width = 200; // Just a narrow road, whee.
 
 const context = canvas.getContext('2d');
 const road = new Road(canvas.width / 2, canvas.width * 0.9);
-const car = new Car(road.getLaneCenter(1), 100, 30, 50, "KEYS", "blue", 6);
+const car = new Car(road.getLaneCenter(1), 100, 30, 50, "AI", "blue", 6);
 
 const traffic = [
     new Car(road.getLaneCenter(1), -100, 30, 50, "DUMMY"),
