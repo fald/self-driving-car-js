40e614583654c8a9ead5a4f99418b14f15131f4c
fald <4348649+fald@users.noreply.github.com>
2022-05-26 16:19:54 -0400

    Main: Refactored to have N AI agents at a time.

--- main.js
+++ main.js
@@ -7,8 +7,10 @@ networkCanvas.width = 300;
 const carContext = carCanvas.getContext('2d');
 const networkContext = networkCanvas.getContext('2d');
 
+const N = 100;
+
 const road = new Road(carCanvas.width / 2, carCanvas.width * 0.9);
-const car = new Car(road.getLaneCenter(1), 100, 30, 50, "AI", "blue", 6);
+const cars = generateCars(N);
 
 const traffic = [
     new Car(road.getLaneCenter(1), -100, 30, 50, "DUMMY"),
@@ -17,28 +19,45 @@ const traffic = [
 animate();
 
 
+function generateCars(N) {
+    const cars = [];
+
+    for (let i = 1; i < N; i++) {
+        cars.push(
+            new Car(road.getLaneCenter(1), 100, 30, 50, "AI", "blue", 6)
+        );
+    }
+
+    return cars;
+}
+
+
 function animate() {
     for (let i = 0; i < traffic.length; i++) {
         traffic[i].update(road.borders, []);
     }
 
-    car.update(road.borders, traffic);
+    for (let i = 0; i < cars.length; i++) {
+        cars[i].update(road.borders, traffic);
+    }
 
     carCanvas.height = window.innerHeight;
     networkCanvas.height = window.innerHeight;
 
     carContext.save();
-    carContext.translate(0, carCanvas.height * 0.7 -car.y);
+    carContext.translate(0, carCanvas.height * 0.7 -cars[0].y);
 
     road.draw(carContext);
     for (let i = 0; i < traffic.length; i++) {
         traffic[i].draw(carContext);
     }
-    car.draw(carContext);
+    for (let i = 0; i < cars.length; i++) {
+        cars[i].draw(carContext);
+    }
 
     carContext.restore();
 
-    Visualizer.drawNetwork(networkContext, car.brain);
+    Visualizer.drawNetwork(networkContext, cars[0].brain);
 
     requestAnimationFrame(animate);
 }
