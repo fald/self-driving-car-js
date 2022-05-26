eed885757d3450968fc30f99146c477b48143420
fald <4348649+fald@users.noreply.github.com>
2022-05-26 14:41:08 -0400

    Main: 'traffic' car's AI is now 'DUMMY', though it acts the same.

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const road = new Road(canvas.width / 2, canvas.width * 0.9);
 const car = new Car(road.getLaneCenter(1), 100, 30, 50, "KEYS", "blue", 6);
 
 const traffic = [
-    new Car(road.getLaneCenter(1), -100, 30, 50, "AI"),
+    new Car(road.getLaneCenter(1), -100, 30, 50, "DUMMY"),
 ];
 
 animate();
