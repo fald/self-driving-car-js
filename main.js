ca19dce13e4d8a0e441770bbbc8b53155365eb40
fald <4348649+fald@users.noreply.github.com>
2022-05-26 16:37:52 -0400

    Brain now applied to car if it exists at the beginning.

--- main.js
+++ main.js
@@ -13,6 +13,9 @@ const road = new Road(carCanvas.width / 2, carCanvas.width * 0.9);
 const cars = generateCars(N);
 
 let bestCar = cars[0];
+if (localStorage.getItem("bestBrain")) {
+    bestCar.brain = JSON.parse(localStorage.getItem("bestBrain"));
+}
 
 const traffic = [
     new Car(road.getLaneCenter(1), -100, 30, 50, "DUMMY"),
